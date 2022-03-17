2cae7591d8017b4f496c18cb9198071506afeab8
YashDaga17 <dagayash17@gmail.com>
2022-03-17 22:28:17 +0530

    Adding Files

---
+++ entry/src/main/js/default/pages/index/index.js
@@ -0,0 +1,12 @@
+// xxx.js
+export default {
+    data: {
+        weather: '/common/images/partly sunny.png',
+        one: '/common/images/1.png',
+        two: '/common/images/2.png',
+        three: '/common/images/3.png',
+
+
+
+    },
+}
